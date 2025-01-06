8490b3d9657771dc7d0a871a367797513b54ef96
Simon Brodtmann <github@accounts.brodtmann.info>
2025-01-06 21:18:54 +0100

    Make item order configurable

--- mf-core/lib/ItemBuilder.lua
+++ mf-core/lib/ItemBuilder.lua
@@ -4,7 +4,8 @@
 local Builder = require("Builder")
 
 local ItemBuilder = Builder:new({
-    _weight = 200 * kg
+    _weight = 200 * kg,
+    _order = "a"
 })
 
 --- Sets the weight of the item calculated from given count per rocket.
@@ -12,6 +13,14 @@ local ItemBuilder = Builder:new({
 --- @return ItemBuilder
 function ItemBuilder:itemsPerRocket(count)
     self._weight = (1000 / count) * kg
+    return self
+end
+
+--- Sets the order string (letter) of the item. The first letter will be a, b or c for the transport belt, underground belt and splitter. The second will be the letter defined here.
+--- @param order string The order string (default: "a")
+--- @return ItemBuilder
+function ItemBuilder:order(order)
+    self._order = order
     return self
 end
 
